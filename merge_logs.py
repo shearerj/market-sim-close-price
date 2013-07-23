c5ba06522f0eb7bdc284bb87485c049f2881c432
Erik Brinkman <ebrink@umich.edu>
2013-07-23 17:53:41 -0400

    added merge_logs script that will merge logs so that the timing of events is lined up

---
+++ merge_logs.py
@@ -0,0 +1,58 @@
+import sys
+from sys import argv
+import os.path
+import re
+from Queue import PriorityQueue
+
+"""
+Merges log files. This is very unsafe in terms of file handling, but if it's
+only used as a one time script it should work fine.
+"""
+
+retime = re.compile(r'\d+\|(\d+)')
+
+class LogReader:
+    def __init__(self, f):
+        self.file = f
+        self.line = f.readline()
+        self.time = -1
+
+    def nextline(self):
+        line = self.line
+        self.line = self.file.readline()
+        match = retime.match(self.line)
+        if match:
+            self.time = int(match.group(1))
+        elif self.time >= 0:
+            self.time = sys.maxint
+        return line
+
+    def __lt__(self, other):
+        return self.time < other.time
+
+    def __eq__(self, other):
+        return self.time == other.time
+
+def merge(logs):
+    "Merges log files off of time. Takes a generator of file descriptors"
+    queue = PriorityQueue()
+    for log in logs:
+        queue.put(LogReader(log))
+
+    while not queue.empty():
+        reader = queue.get()
+        line = reader.nextline()
+        if not line:
+            continue
+        sys.stdout.write(line)
+        queue.put(reader)
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print "Merges various log files for easy comparison"
+        print ""
+        print "Usage: python merge_logs.py [log files] > new_log_file"
+        print "       python merge_logs.py <log_dir>/* > new_log_file"
+        sys.exit(1)
+    merge(open(f) for f in argv[1:])
+    
