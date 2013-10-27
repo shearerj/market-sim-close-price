c378b305ebb3f07079d8b98b667f727543eeb968
Alex Ng <alexng@umich.edu>
2013-10-27 15:54:31 -0400

    OrderData class

---
+++ src/data/OrderData.java
@@ -0,0 +1,55 @@
+package data;
+
+import java.util.Random;
+
+import entity.agent.PrivateValue;
+import entity.infoproc.SIP;
+import entity.market.Market;
+import entity.market.Price;
+import event.TimeStamp;
+
+public class OrderData {
+	private TimeStamp timestamp;
+	private Price price;
+	private int quantity;
+	boolean isBuy;
+	
+	public TimeStamp getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(TimeStamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Price getPrice() {
+		return price;
+	}
+
+	public void setPrice(Price price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public boolean isBuy() {
+		return isBuy;
+	}
+
+	public void setBuy(boolean isBuy) {
+		this.isBuy = isBuy;
+	}
+
+	public OrderData(TimeStamp timestamp, Price price, int quantity, boolean isBuy) {
+		this.timestamp = timestamp;
+		this.price = price;
+		this.quantity = quantity;
+		this.isBuy = isBuy;
+	}
+}
