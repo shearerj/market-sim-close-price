763f6e5a7b8072fde5b8636fea687bd65b6c7891
Harsha Patankar <harshayp@umich.edu>
2013-11-16 15:56:39 -0500

    checking edge cases

--- test/utils/MathUtilsTest.java
+++ test/utils/MathUtilsTest.java
@@ -1,7 +1,5 @@
 package utils;
 
-
-
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,9 +10,7 @@ import org.junit.Test;
 
 public class MathUtilsTest  extends MathUtils
 {
-	
-	
-	
+			
 	
 	@Test
 	public void testingLogn() {
@@ -27,13 +23,16 @@ public class MathUtilsTest  extends MathUtils
 		assertTrue(result==1);//normal test case
 
 		result = MathUtils.logn(number, base);
-		assert(result==1);//normal test case
+		assertTrue(result==1);//normal test case
 
 		//log base 10 of 10 is 1
 		number = 10;
 		base = 10;
 		result = logn(number, base);
 		assertTrue(result==1);
+		
+		//boundary cases of number <= 0, as ln is undefined for those values
+		//these ensure -1 is given where log n is undefined
 		number = 0;
 		base = 10;
 		result = logn(number, base);
@@ -41,22 +40,27 @@ public class MathUtilsTest  extends MathUtils
 		number=-999;
 		result = logn(number,base);
 		assertTrue(result==-1);
-		//boundry cases of number <= 0, as ln is undefined for those values
-		//ensure -1 is given where log n is undefined
+		
 
-		//pathalogical test cases like
-		//number = .000000000000001; wouldnt apply as number is of type int
+		//Pathological test cases like
+		//number = .000000000000001; wouldn't apply as number is of type int
 		
 		
 		//shows that logn function provided allows use of negative base values
 		number = 10;
 		base = -1; 
 		result = logn(number, base);
-		assertTrue(result!=-1);
+		// 10/-1=-10, log =0, hence result is 0
+		//it appears that in general, if the base is negative while the number is positive
+		// result returned will be 0
+		assertTrue(result==0);
+		
 		number=-2;
 		result = logn(number,base);
+		//as number provided is already negative, would not enter loop, returning -1
 		assertTrue(result==-1);
 		
+		//show -1 is given if and only if starting number is already negative or zero
 		number = 0;
 		base = 2;
 		result = logn(number, base);
@@ -65,29 +69,42 @@ public class MathUtilsTest  extends MathUtils
 		result = logn(number,base);
 		assertTrue(result==-1);
 		
-		number = 5; base =3;
+		number = 5; base = 3;
 		//if using double answer should be  0.69897000434
 		result = logn(number,base);
+		//however integer math forces result to be rounded to 1
 		assertTrue(result==1);
 		
-		//7 log 9 is  0.69897000434
-		number =7; 
-		base =9;
+		//7 log 9 is   0.88562187458
+		number = 7; 
+		base = 9;
 		result = logn(number,base);
-		assert(result==1);
+		assertTrue(result!=1);
+		//as 7 > 0, 7/9 = 0.7778.. gets cut to 0 
+		//stopping the incrementing of log at 0
+		assertTrue(result==0);
 		
-		//62 log e is  4.127134385
+		//62 log e is  4.127134385,
+		//this is rounded due to repeated integer division to 3
 		number =62; 
 		base =3;//rounding as input is integer base
 		result = logn(number,base);
-		assert(result==4);
+		assertTrue(result!=4);
+		//62/3=20.666, this is cut/truncated to 20, 
+		//20/3 cut to 6, 6/3 is 2, 2/3 is cut to 0
+		//resulting in result of -1+1(4) = 3
+		//example of cases in which answer differs from double variant by more than 1 
+		assertTrue(result==3);
+		
+		
 		
 		//39 log 37 is  1.591064607 
 		number =39; 
 		base =37;
 		result = logn(number,base);
-		assert(result==2);
-		
+		assertTrue(result!=2);
+		// results of repeated integer divisions, result in  1.59 being truncated to 1
+		assertTrue(result==1);
 	}
 
 	@Test
@@ -97,18 +114,18 @@ public class MathUtilsTest  extends MathUtils
 		int n = 1001;
 		int quanta= 10;
 		int q = quantize(n, quanta);
-		assert(q==1000);
+		assertTrue(q==1000);
 		
-		//to verify function rounds down for negative numbers
+		//used to verify function rounds to positive infinity for negative numbers
 		n = -32; 
 		q = quantize(n, quanta);
-		assert(q==-30);
+		assertTrue(q==-30);
 		
 		//verifies that when provided a value that is a multiple of the quanta
 		//the program will round to the same value
 		n=0;
 		q = quantize(n, quanta);
-		assert(q==0);
+		assertTrue(q==0);
 	}
 	
 	@Test
@@ -118,20 +135,25 @@ public class MathUtilsTest  extends MathUtils
 		double n = 100.003;
 		double quanta= 10;
 		double qresult = quantize(n, quanta);
-		assert(qresult==100);
+		assertTrue(qresult==100);
 		
 		//verifies that n is not rounded incorrectly
-		//verifies that  negative numbers below middle round down to negative quanta
+		//verifies that  negative numbers below middle round up to quanta closer to 0
 		n = -34.99999; 
 		qresult = quantize(n, quanta);
-		assert(qresult==-30);
+		assertTrue(qresult==-30);
+		
+		//in cases where the double n is closer to the lower quanta, quantize rounds down to lower quanta
+		n=-35.999999;
+		qresult = quantize(n, quanta);
+		assertTrue(qresult==-40);
 
 		//checking that  when provided a value that is a multiple of the quanta
 		//the program will round to the same value
 		n=-0.000000000000000000;
 		qresult = quantize(n, quanta);
-		assert(qresult==0);
-		//assert(n==0)// also can be used to check floating point accuracy
+		assertTrue(qresult==0);
+		//assertTrue(n==0)// also can be used to check floating point accuracy
 
 	}
 
@@ -145,7 +167,7 @@ public class MathUtilsTest  extends MathUtils
 		int lower = 1;
 		int upper = 2;
 		int b = bound(num,lower, upper);
-		assert(b==1);
+		assertTrue(b==1);
 		
 		//testing valid results using repeated values 
 		// for variables
@@ -153,22 +175,22 @@ public class MathUtilsTest  extends MathUtils
 		lower = 0;
 		upper = 1;
 		b = bound(num,lower, upper);
-		assert(b==0);
+		assertTrue(b==0);
 		
 		num=0; lower = 1; upper =0;
 		b = bound(num,lower, upper);
-		assert(b==0);
+		assertTrue(b!=0);
 		
 		num=0; lower = 0; upper =1;
 		b = bound(num,lower, upper);
-		assert(b==0);
+		assertTrue(b==0);
 		
 		//verifying case when all three inputs provided are same
 		num = 0;
 		lower = 0;
 		upper = 0;
 		b = bound(num,lower, upper);
-		assert(b==0);
+		assertTrue(b==0);
 		
 		//testing with negative numbers and large values provided
 		num = -999999899;
@@ -176,7 +198,7 @@ public class MathUtilsTest  extends MathUtils
 		lower = -88;
 		upper = -875;
 		b = bound(num,lower, upper);
-		assert(b==-88);
+		assertTrue(b==-88);
 		
 		//testing using inputs for largest integer values possible
 		int numLargest =  2147483647; //testing with likely largest number storable in Java x32
@@ -184,7 +206,7 @@ public class MathUtilsTest  extends MathUtils
 		lower = -num; //using smallest number possible in Java
 		upper = num;
 		b = bound(num,lower, upper);
-		assert(b==num);
+		assertTrue(b==num);
 		upper = -numLargest;
 		b = bound(num,lower, upper);
 		assertTrue(b==upper);
