91e92d36dfcb868c806aaa5717cdc560b7aeda6f
Erik Brinkman <ebrink@umich.edu>
2013-12-05 10:34:23 -0500

    Made scripts a little better

--- run-hft-presets.sh
+++ run-hft-presets.sh
@@ -4,25 +4,30 @@
 if [ $# -lt 2 ]; then
     echo "Run simulation spec with all of the specified presets"
     echo
-    echo "Usage: $0 directory num-obs"
+    echo "Usage: $0 directory num-obs [preset1]..."
     echo
     echo "Creates a new directory for each preset, and runs the spec file num-obs times"
-    echo "each, then merges each of the obsevations into a new directory called merged"
+    echo "each, then merges each of the obsevations into a new directory called merged."
+    echo "Presets can be optionally specified after num-obs. If omitted this script will"
+    echo "use the default presets of CENTRALCALL, CENTRALCDA, TWOMARKET, and TWOMARKETLA."
     exit 1
+elif [ $# -lt 3 ]; then
+    PRESETS=( CENTRALCALL CENTRALCDA TWOMARKET TWOMARKETLA )
+else
+    PRESETS=( "${@:3}" )
 fi
-
-PRESETS=( CENTRALCALL CENTRALCDA TWOMARKET TWOMARKETLA )
 FILE=simulation_spec.json
 MERGED=merged
 
 FOLDER=$1
 NUM=$2
 
-for PRESET in ${PRESETS[@]}; do
+for I in "${!PRESETS[@]}"; do 
+    PRESET="${PRESETS[$I]}"
     echo ">> Setting up ${PRESET,,}"
     mkdir -vp "$FOLDER/$PRESET"
     cp -v "$FOLDER/$FILE" "$FOLDER/$PRESET/$FILE"
-    sed -i -e 's/"presets" *: *"[^"]*"/"presets": "'"$PRESET"'"/g' -e 's/"modelName" *: *"[^"]*"/"modelName": "'"${PRESET,,}"'"/g' "$FOLDER/$PRESET/$FILE"
+    sed -i -e 's/"presets" *: *"[^"]*"/"presets": "'"$PRESET"'"/g' -e 's/"modelName" *: *"[^"]*"/"modelName": "'"${PRESET,,}"'"/g' -e 's/"modelNum" *: *"[^"]*"/"modelNum": "'"${I}"'"/g' "$FOLDER/$PRESET/$FILE"
     ./run-hft.sh "$FOLDER/$PRESET" $NUM
 done
 
