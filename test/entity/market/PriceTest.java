12a1db6459f7623dcba893bed45a4b13cc1dd389
Elaine Wah <ewah@umich.edu>
2014-01-27 13:41:04 -0500

    Fixing inf price ticks bug

--- test/entity/market/PriceTest.java
+++ test/entity/market/PriceTest.java
@@ -15,5 +15,13 @@ public class PriceTest {
 		assertEquals("-$100.01", new Price(-100010).toString());
 		assertEquals("-$100.001", new Price(-100001).toString());
 	}
+	
+	@Test
+	public void infinitePriceTest() {
+		// note INF prices occur in the AA strategy
+		assertEquals(Price.INF, new Price((double) Integer.MAX_VALUE));
+		assertEquals(Price.INF, new Price((double) Integer.MAX_VALUE+1));
+		assertEquals(Price.INF, new Price((double) Integer.MAX_VALUE+3));
+	}
 
 }
