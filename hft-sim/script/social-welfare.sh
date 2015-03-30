71c4da4b8eb44076ff29382baa1af0353e636f41
Erik Brinkman <ebrink@umich.edu>
2015-03-29 22:35:46 -0400

    Updated social welfare to allow arbitrary jar files

--- hft-sim/script/social-welfare.sh
+++ hft-sim/script/social-welfare.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo "usage: $0 <simspec> <role-counts> <profile> <numsamples> <numprocs>"
+    echo "usage: $0 <simspec> <role-counts> <profile> <numsamples>"
+    echo "           <numprocs> [jar]"
     exit
 elif [[ "$#" -lt "3" ]]; then
-    echo "usage: $0 <simspec> <role-counts> <profile> <numsamples> <numprocs>"
+    echo "usage: $0 <simspec> <role-counts> <profile> <numsamples>"
+    echo "           <numprocs> [jar]"
     exit 1
 fi
 
@@ -15,15 +17,15 @@ NSAMP="${4:-1}"
 PROCS="${5:-1}"
 DIR="$(dirname "$0")"
 TMPDIR="$(mktemp -d)" || (echo "couldn't make temporary directory"; exit 1)
+if [[ -z "$6" ]]; then
+    EXEC=("$DIR/run-hft.sh")
+else
+    EXEC=("$DIR/run-hft-local.sh" "$6")
+fi
 
 "$DIR/sample.py" -s "$SPEC" -r "$ROLECOUNT" -f "$PROFILE" \
     -n "$NSAMP" -d "$TMPDIR"
 find "$TMPDIR" -mindepth 1 -maxdepth 1 -type d -print0 \
-    | xargs -0n1 -P "$PROCS" "$DIR/run-hft.sh" \
-    2>/dev/null
-if [[ "$?" -ne "0" ]]; then
-    echo "Error running simulations" 1>2
-    rm -rf "$TMPDIR"
-fi
+    | xargs -0n1 -P"$PROCS" "${EXEC[@]}"
 "$DIR/average-social-welfare.py" "$TMPDIR"/*/observation*.json
 rm -rf "$TMPDIR"
