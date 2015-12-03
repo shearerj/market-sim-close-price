b6a106f2ab90896ab287eb7f3691014bf3572077
Erik Brinkman <erik.brinkman@gmail.com>
2015-12-02 23:55:15 -0500

    Update run_egta.sh to use 1000 numsims

--- market-sim/docs/jq.md
+++ market-sim/docs/jq.md
@@ -7,7 +7,7 @@ that contains a bunch of common queries, that can all be invoked by `jq -f
 jq/<filename>.jq`. Below is a description of some of them.
 
 Expand Strategies (`expand_strats.jq`)
-====================================
+======================================
 
 This filter takes as input an egta simulation spec file, except in assignment
 instead of a list of strategies, the input is a dictionary of strategy to
@@ -31,14 +31,14 @@ large number of one type of agent. An example input looks like:
 
 
 Social Welfare (`social_welfare.jq`)
-==================================
+====================================
 
 This filter takes as input an observation file (or sequence of them), and
 outputs the social welfare (sum of all of the player payoffs) of each
 observation.
 
 Role Strategy Payoffs (`role_strat_payoffs.jq`)
-=============================================
+===============================================
 
 This filter takes as input an observation and returns an object mapping roles
 to strategies to average payoff for that role strategy pair. An example output
@@ -53,3 +53,12 @@ looks like:
     ...
 }
 ```
+
+Merge Payoffs (`merge_payoffs.jq`)
+==================================
+
+This filter takes as input an array of observation files (or the standard
+output with the slurp (`-s`) option, and merges them into one observation file
+with the average payoff for each player. Alternatively, one can group
+observations together, in which case this can be used somewhat like numsims to
+condense input.
--- market-sim/run-egta.sh
+++ market-sim/run-egta.sh
@@ -9,7 +9,7 @@
 # Fail if bad stuff happens
 set -euf -o pipefail
 
-# Chec that appropriate number of parameters is specified
+# Check that appropriate number of parameters is specified
 [ "$#" != "2" ] && echo 'Must supply "directory" and "num observations"' 1>&2 && exit 1
 
 # Current directory
@@ -21,5 +21,19 @@ if [[ "$(hostname)" == *'arc-ts.umich.edu' ]]; then
     module load coreutils
 fi
 
+# This is the number of observations to condense into one observation
+NUMSIMS=1000
+
 # Execution
-java -Xms1G -Xmx4G -jar "$DIR/target/marketsim-4.0.0-jar-with-dependencies.jar" --egta -s "$1/simulation_spec.json" "$2" | split -a4 --additional-suffix=.json -l 1 - "$1/observation_"
+java -Xms1G -Xmx4G -jar "$DIR/target/marketsim-4.0.0-jar-with-dependencies.jar" --egta -s "$1/simulation_spec.json" "$(($NUMSIMS * $2))" \
+    | sed -e "1~${NUMSIMS}s#^#[#" -e "0~${NUMSIMS}"'s#$#]#' -e "0~${NUMSIMS}!"'s#$#,#' \
+    | jq -c -f "$DIR/jq/merge_payoffs.jq" \
+    | split -a4 --additional-suffix=.json -l 1 - "$1/observation_"
+
+# `paste` and `jq` are what do the numsims aggregation. `paste` is used to
+# concat NUMSIMS observations on a single line separated by the record
+# delimiter. This is then passed into jq which then splits each line into an
+# array of json files. This allows the aggregation to be done in parallel.
+# Finally, merge_payoffs takes every array and merges it into a single array,
+# thus completing the split. The final jq has thw `-c` option putting one
+# result on a line before it's split into one line per file with `split`
