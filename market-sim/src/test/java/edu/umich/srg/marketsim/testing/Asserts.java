4d7a7f83b2cca012f07a84c46057e8a8ba5f2853
Erik Brinkman <erik.brinkman@gmail.com>
2015-12-07 17:41:55 -0500

    Fix assertQuote failure

--- market-sim/src/test/java/edu/umich/srg/marketsim/testing/Asserts.java
+++ market-sim/src/test/java/edu/umich/srg/marketsim/testing/Asserts.java
@@ -3,21 +3,21 @@ package edu.umich.srg.marketsim.testing;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.junit.Assert.assertEquals;
 
+import com.google.common.base.Optional;
 import com.google.common.collect.Sets;
 import com.google.common.collect.Sets.SetView;
 
 import edu.umich.srg.marketsim.Price;
 import edu.umich.srg.marketsim.market.Quote;
 
-import java.util.Optional;
 import java.util.Set;
 
 public class Asserts {
 
   /** Prices are null for absent prices (for convenience) */
   public static void assertQuote(Quote quote, Price bid, Price ask) {
-    assertEquals("Incorrect ASK", Optional.ofNullable(ask), quote.getAskPrice());
-    assertEquals("Incorrect BID", Optional.ofNullable(bid), quote.getBidPrice());
+    assertEquals("Incorrect ASK", Optional.fromNullable(ask), quote.getAskPrice());
+    assertEquals("Incorrect BID", Optional.fromNullable(bid), quote.getBidPrice());
   }
 
   public static <T> void assertSetEquals(Set<? extends T> expected, Set<? extends T> actual) {
