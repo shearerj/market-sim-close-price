91fdac7932c6e9600c15cc1f96c4be71e4bcaac2
Erik Brinkman <erik.brinkman@gmail.com>
2018-05-14 11:36:13 -0400

    egta dynamically responds to mem

--- market-sim/run-egta.sh
+++ market-sim/run-egta.sh
@@ -14,16 +14,18 @@ set -euf -o pipefail
 
 # Current directory
 DIR="$(dirname "$0")"
+MEM=8192
 
 # Check if on flux. If so add java 8 to path
 if [[ "$(hostname)" == *'arc-ts.umich.edu' ]]; then
     export PATH="/nfs/wellman_ls/bin/:$PATH"
+    MEM=$(( $(qstat "$PBS_JOBID" -M | tail -n1 | tr -s ' ' | cut -d' ' -f8 | rev | cut -c3- | rev) * 8 - 512))
 fi
 
 # This is the number of observations to condense into one observation
 SIMS_PER_OBS="$(jq '.configuration.numSims // 1000 | tonumber' "$1/simulation_spec.json")"
 
 # Execution
-java -Xms1G -Xmx4G -jar "$DIR/target/marketsim-4.0.0-jar-with-dependencies.jar" --no-features --jobs 1 --sims-per-obs "$SIMS_PER_OBS" --spec <(< "$1/simulation_spec.json" jq 'del(.configuration.numSims)' | jq -f "$DIR/jq/reduce_strats.jq") "$2" \
+java -ms1024m -mx${MEM}m -jar "$DIR/target/marketsim-4.0.0-jar-with-dependencies.jar" --no-features --jobs 1 --sims-per-obs "$SIMS_PER_OBS" --spec <(< "$1/simulation_spec.json" jq 'del(.configuration.numSims)' | jq -f "$DIR/jq/reduce_strats.jq") "$2" \
     | jq -c . \
     | split -a4 --additional-suffix=.json -l 1 - "$1/observation_"
