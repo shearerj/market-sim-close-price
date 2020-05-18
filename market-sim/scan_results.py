28990e8fd4f4d81fe1b5f4d3281f9a4af3259fc1
Megan Shearer <shearerj@Megans-MacBook-Pro-2.local>
2020-05-18 11:03:39 -0400

    scan results 5

--- market-sim/scan_results.py
+++ market-sim/scan_results.py
@@ -1,16 +1,19 @@
 import json
 import numpy as np
+from functools import reduce
 
 def main():
     stats = {}
+    paths = {}
 
     for i in range(200):
     	entry = f'out-{i}'
     	with open(f'drl_env1/output-{i}.json') as json_file:
     		stats[entry] = json.load(json_file)
+    		paths[entry] = ['Benchmark-Manipulator', 'mean']
 
-    print(max(dict, key=lambda x: (dict[x].get('Benchmark-Manipulator')).get('mean')))
-    #print(stats)
+
+	print(max(stats, key=lambda k: value(k, stats, paths[k])))
 
 
 if __name__ == '__main__':
