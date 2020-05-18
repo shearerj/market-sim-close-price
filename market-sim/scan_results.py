77f3554ea1f44ca1c908ac5b605db45ac548470a
Megan Shearer <shearerj@Megans-MacBook-Pro-2.local>
2020-05-18 10:40:24 -0400

    scan results 1

---
+++ market-sim/scan_results.py
@@ -0,0 +1,15 @@
+import json
+import numpy as np
+
+def main():
+    stats = {}
+
+    for i in range(200):
+    	entry = f'out-{i}'
+        stats[entry] = json.load(f'drl_env1/output-{i}.json')
+
+    print(stats)
+
+
+if __name__ == '__main__':
+    main()
